bc947b5162e887d387158f8b194bd17396ae2597
Tin Truong <truongtrongtin0305@gmail.com>
2022-05-14 22:59:03 +0700

    feat: add admin mode

--- src/listeners/actions/app-home-new-absence.ts
+++ src/listeners/actions/app-home-new-absence.ts
@@ -1,4 +1,5 @@
 import { App } from '@slack/bolt';
+import { hasAdminRole } from '../../helpers';
 import newAbsenceModal from '../../user-interface/modals/new-absence';
 
 export default function appHomeNewAbsence(app: App) {
@@ -7,12 +8,17 @@ export default function appHomeNewAbsence(app: App) {
     async ({ ack, body, client, logger }) => {
       // Acknowledge the button request
       await ack();
+      const userInfo = await client.users.info({ user: body.user.id });
+      const email = userInfo.user?.profile?.email;
+      const isAdmin = hasAdminRole(email!);
+      const realName = userInfo.user?.profile?.real_name;
+      logger.info(`${realName} is opening new absence modal from app home`);
 
       try {
         // Call views.update with the built-in client
         await client.views.open({
           trigger_id: body.trigger_id,
-          view: newAbsenceModal(),
+          view: newAbsenceModal(isAdmin, body.user.id),
         });
       } catch (error) {
         logger.error(error);
