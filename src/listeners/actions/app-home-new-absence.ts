74c5c0e19280f4a9a0bdfbfc69146b0d6682cc29
Tin Truong <truongtrongtin0305@gmail.com>
2022-05-16 20:07:39 +0700

    chore: log when opening new absence modal

--- src/listeners/actions/app-home-new-absence.ts
+++ src/listeners/actions/app-home-new-absence.ts
@@ -14,6 +14,10 @@ export default function appHomeNewAbsence(app: App) {
           trigger_id: body.trigger_id,
           view: newAbsenceModal(),
         });
+
+        const userInfo = await client.users.info({ user: body.user.id });
+        const realName = userInfo.user?.profile?.real_name;
+        logger.info(`${realName} is opening new absence modal from app home`);
       } catch (error) {
         logger.error(error);
       }
