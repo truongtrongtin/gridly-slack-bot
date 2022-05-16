b2a7951349b57c4e647dbbba56d882e5d701ebfb
Tin Truong <truongtrongtin0305@gmail.com>
2022-05-16 16:07:39 +0700

    chore: log when opening new absence modal

--- src/listeners/shortcuts/global-new-absence.ts
+++ src/listeners/shortcuts/global-new-absence.ts
@@ -5,13 +5,19 @@ export default function globalNewAbsence(app: App) {
   app.shortcut(
     'register_absences',
     async ({ shortcut, ack, client, logger }) => {
-      await ack();
-
       try {
+        await ack();
+
         await client.views.open({
           trigger_id: shortcut.trigger_id,
           view: newAbsenceModal(),
         });
+
+        const userInfo = await client.users.info({ user: shortcut.user.id });
+        const realName = userInfo.user?.profile?.real_name;
+        logger.info(
+          `${realName} is opening new absence modal from global shortcut`,
+        );
       } catch (error) {
         logger.error(error);
       }
