a27e5426699b345bb16a1d11693090d97f4e60ef
Tin Truong <tin@localizedirect.com>
2022-05-16 10:48:19 +0700

    fix: revert admin mode

--- src/listeners/shortcuts/global-new-absence.ts
+++ src/listeners/shortcuts/global-new-absence.ts
@@ -1,24 +1,16 @@
 import { App } from '@slack/bolt';
-import { hasAdminRole } from '../../helpers';
 import newAbsenceModal from '../../user-interface/modals/new-absence';
 
 export default function globalNewAbsence(app: App) {
   app.shortcut(
     'register_absences',
     async ({ shortcut, ack, client, logger }) => {
-      try {
-        await ack();
-        const userInfo = await client.users.info({ user: shortcut.user.id });
-        const email = userInfo.user?.profile?.email;
-        const isAdmin = hasAdminRole(email!);
-        const realName = userInfo.user?.profile?.real_name;
-        logger.info(
-          `${realName} is opening new absence modal from global shortcut`,
-        );
+      await ack();
 
+      try {
         await client.views.open({
           trigger_id: shortcut.trigger_id,
-          view: newAbsenceModal(isAdmin, shortcut.user.id),
+          view: newAbsenceModal(),
         });
       } catch (error) {
         logger.error(error);
