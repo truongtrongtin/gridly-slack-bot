d6b6db72c3334db1a07576b242bdebb647d19649
Tin Truong <truongtrongtin0305@gmail.com>
2022-12-30 11:25:26 +0700

    chore: update tsconfig

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ const expressReceiver = new ExpressReceiver({
 });
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN!,
   receiver: expressReceiver,
   processBeforeResponse: isOnGoogleCloud,
   logLevel: LogLevel.INFO,
