8b9fe811046b60c2f53aa30e1e7ae55dc377bb01
Tin Truong <truongtrongtin0305@gmail.com>
2024-03-19 14:13:24 +0700

    chore: eslint flat config

--- src/services/getAccessTokenFromRefreshToken.ts
+++ src/services/getAccessTokenFromRefreshToken.ts
@@ -6,21 +6,17 @@ export async function getAccessTokenFromRefreshToken() {
     return accessToken;
   }
   console.log('request new access token');
-  try {
-    const response = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      body: new URLSearchParams({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        grant_type: 'refresh_token',
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-      }),
-    });
-    const tokenObject = await response.json();
-    if (!response.ok) throw tokenObject;
-    accessToken = tokenObject.access_token;
-    return tokenObject.access_token;
-  } catch (error) {
-    throw error;
-  }
+  const response = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      grant_type: 'refresh_token',
+      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+    }),
+  });
+  const tokenObject = await response.json();
+  if (!response.ok) throw tokenObject;
+  accessToken = tokenObject.access_token;
+  return tokenObject.access_token;
 }
--- src/services/getUserInfo.ts
+++ src/services/getUserInfo.ts
@@ -1,14 +1,10 @@
 export async function getUserInfo(accessToken: string) {
-  try {
-    const response = await fetch(
-      'https://www.googleapis.com/oauth2/v3/userinfo',
-      { headers: { Authorization: `Bearer ${accessToken}` } },
-    );
-    const userInfo = await response.json();
-    if (!response.ok) throw userInfo;
-    console.log(userInfo.name);
-    return userInfo;
-  } catch (error) {
-    throw error;
-  }
+  const response = await fetch(
+    'https://www.googleapis.com/oauth2/v3/userinfo',
+    { headers: { Authorization: `Bearer ${accessToken}` } },
+  );
+  const userInfo = await response.json();
+  if (!response.ok) throw userInfo;
+  console.log(userInfo.name);
+  return userInfo;
 }
